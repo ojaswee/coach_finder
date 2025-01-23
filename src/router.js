18b3fb586124e6e34df2ff131e68b1fa8f6ebbb8
Ojaswee Dhungana <odhungana@itsmail.ca>
2025-01-23 12:18:53 -0800

    inital commit to coachRegistration

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter,createWebHistory } from 'vue-router';
 
 import CoachesDetail from './pages/coaches/CoachesDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachesRegistration from './pages/coaches/CoachesRegistration.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
@@ -18,7 +18,7 @@ const router = createRouter({
 			children:
 				[{ path: 'contact', component: ContactCoach }]
 		},
-	{ path: '/register', component: CoachesRegistration },
+	{ path: '/register', component: CoachRegistration },
 	{ path: '/requests', component: RequestsReceived },
 	{ path: '/:notFound(.*)', component: NotFound },
 	],
